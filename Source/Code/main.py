484b03daaffd166d25b721b4affd990b72104b26
an <154230649+ashwan94@users.noreply.github.com>
2025-03-26 06:00:38 +0900

    테스트

--- Source/Code/main.py
+++ Source/Code/main.py
@@ -1,5 +1,6 @@
-# 250326(Wed)
-# 도서 <이끝삼시> 소개 웹페이지
+from PIL import Image
 
-if __name__ == '__main__':
-    print('오랜만에 성공')
+# 바탕화면 경로에 있는 이미지 불러오기
+image_path = "/Users/na/Desktop/IMG_6840.JPG"  # 바탕화면 경로에 맞게 수정하세요
+image = Image.open(image_path)
+image.show()  # 이미지를 열어서 확인
